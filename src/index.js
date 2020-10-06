180ea01548a22183c89437e584dd8d5a79b39aad
connie.yu <imconnieyu@gmail.com>
2020-10-06 12:40:53 +1000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Background from "./image/product1.jpg";
+import * as serviceWorker from "./serviceWorker";
+
+function Card(props) {
+  return (
+    <div>
+      <h1 className="title">{props.title}</h1>
+      <div className="card">
+        <img className="card__img" src={props.img}></img>
+        <h2 className="card__subtitle">{props.subTitle}</h2>
+        <p className="card__desc">{props.description}</p>
+      </div>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Card
+    title="Editor's Picks"
+    img={Background}
+    subTitle="Gifts for Her"
+    description="For Valentine's Day"
+  />,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
